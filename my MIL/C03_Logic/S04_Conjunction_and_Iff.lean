58d4d37abc26c171f81af2fe0b85e471af07c383
Daniel <Dan814@uw.edu>
2024-04-10 20:56:51 -0700

    Continued work on 3.4

--- my MIL/C03_Logic/S04_Conjunction_and_Iff.lean
+++ my MIL/C03_Logic/S04_Conjunction_and_Iff.lean
@@ -63,8 +63,14 @@ example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x := by
 example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x :=
   fun h' ↦ h.right (le_antisymm h.left h')
 
-example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) : m ∣ n ∧ ¬n ∣ m :=
-  sorry
+example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) : m ∣ n ∧ ¬n ∣ m := by
+ rcases h with ⟨h0, h1⟩
+ constructor
+ . assumption
+ intro h2
+ apply h1
+ apply Nat.dvd_antisymm h0 h2
+
 
 example : ∃ x : ℝ, 2 < x ∧ x < 4 :=
   ⟨5 / 2, by norm_num, by norm_num⟩
@@ -101,11 +107,10 @@ example {x y : ℝ} (h : x ≤ y) : ¬y ≤ x ↔ x ≠ y := by
 example {x y : ℝ} (h : x ≤ y) : ¬y ≤ x ↔ x ≠ y :=
   ⟨fun h₀ h₁ ↦ h₀ (by rw [h₁]), fun h₀ h₁ ↦ h₀ (le_antisymm h h₁)⟩
 
-example {x y : ℝ} : x ≤ y ∧ ¬y ≤ x ↔ x ≤ y ∧ x ≠ y :=
-  sorry
+example {x y : ℝ} : x ≤ y ∧ ¬y ≤ x ↔ x ≤ y ∧ x ≠ y := sorry
 
 theorem aux {x y : ℝ} (h : x ^ 2 + y ^ 2 = 0) : x = 0 :=
-  have h' : x ^ 2 = 0 := by sorry
+  have h' : x ^ 2 = 0 := by linarith [pow_two_nonneg x, pow_two_nonneg y]
   pow_eq_zero h'
 
 example (x y : ℝ) : x ^ 2 + y ^ 2 = 0 ↔ x = 0 ∧ y = 0 :=
@@ -130,7 +135,11 @@ theorem not_monotone_iff {f : ℝ → ℝ} : ¬Monotone f ↔ ∃ x y, x ≤ y �
   rfl
 
 example : ¬Monotone fun x : ℝ ↦ -x := by
-  sorry
+rw[not_monotone_iff]
+use 0
+use 1
+norm_num
+
 
 section
 variable {α : Type*} [PartialOrder α]
@@ -138,7 +147,19 @@ variable (a b : α)
 
 example : a < b ↔ a ≤ b ∧ a ≠ b := by
   rw [lt_iff_le_not_le]
-  sorry
+  constructor
+  rintro ⟨h₀, h₁⟩
+  constructor
+  . assumption
+  . intro h
+    apply h₁
+    rw[h]
+  rintro ⟨h₀, h₁⟩
+  constructor
+  . assumption
+  . intro h
+    apply h₁
+    exact le_antisymm h₀ h
 
 end
 
